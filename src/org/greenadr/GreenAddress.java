f22f63f219cccb3918fe1dc51ef1d7722ea7b499
unknown <mohamedsheik16>
2021-02-26 14:39:32 +0530

    Syed

---
+++ src/org/greenadr/GreenAddress.java
@@ -0,0 +1,45 @@
+package org.greenadr;
+
+public class GreenAddress {
+
+	private void grnadr(String a) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Greens Technology address is:"+"\n"+a);
+
+
+}
+	
+	private void greenord(String m) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Course name is"+m);
+	}
+	
+	private void greenstype(String l) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Course type is"+l);
+
+	}
+	
+	private void greensnum(long a) {
+		// TODO Auto-generated method stub
+
+		System.out.println("OMR phonenumber is"+a);
+	}
+	
+public static void main(String[] args) {
+	
+
+		// TODO Auto-generated method stub
+		
+		GreenAddress g=new GreenAddress();
+		g.grnadr("Rajiv Gandhi Salai (OMR Egattur Village, Navalur Landmark: Just before navalur, Tollgate, Chennai, Tamil Nadu.");
+		g.greenord("Selenium Training");
+		g.greenstype("Online class");
+		g.greensnum(6385149793l);
+}
+
+	}
+
